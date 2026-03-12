4fb83c1071978aaf8e7c958d7756968875b290dd
DIEGO FERNANDO MUÑOZ <diegofmupp23@gmail.com>
2026-03-12 09:49:55 -0500

    sazazasa

---
+++ calculos.R
@@ -0,0 +1,29 @@
+library(AQLSchemes) 
+
+#Plan simple con nivel de inspección Normal
+planS_1<-AASingle('Normal')
+planS_1
+planS_2<-AASingle('Normal')
+planS_2
+planS_3<-AASingle('Normal')
+planS_3
+
+#Plan simple con nivel de inspección Estricto
+planE_1<-AASingle('Tightened')
+planE_1
+planE_2<-AASingle('Tightened')
+planE_2
+planE_3<-AASingle('Tightened')
+planE_3
+
+#Plan simple con nivel de inspección Reducido
+planr_1<-AASingle('Reduced')
+planr_1
+planr_2<-AASingle('Reduced')
+planr_2
+planr_3<-AASingle('Reduced')
+planr_3
+
+#Plan Doble con nivel de inspección Normal
+planD<-AADouble('Normal')
+planD
